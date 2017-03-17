fe6027275a57151128fdacace37ca671f05a1cd8
Christopher Medlin <christopherjmedlin@gmail.com>
2017-03-16 18:03:08 -0700

    Add unfinished WidgetBuilder class.

---
+++ src/main/java/org/usfirst/frc/falcons6443/smashboard/widgets/WidgetBuilder.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.falcons6443.smashboard.widgets;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import org.w3c.dom.Element;
+
+/**
+ * Contains several static methods to build widgets from XML elements.
+ *
+ * @author Christopher Medlin
+ */
+public class WidgetBuilder {
+
+    /**
+     * Builds a SpeedBar from an XML element.
+     *
+     * @param e the XML element
+     * @param ntable the NetworkTable that the resulting SpeedBar will use
+     * @return the resulting SpeedBar
+     */
+    public static SpeedBar speedBarFromXML (Element e, NetworkTable ntable) {
+
+    }
+}
